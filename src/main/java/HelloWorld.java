a9a0575665050f287764e2bdf491c837634d8f16
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2014-11-16 22:13:05 +0900

    add findbugs plugin

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
   public String hello() {
     return "Hello, World!";
   }
+  
+  public boolean stringEqualsAbc(String str) {
+    return "abc" == str;
+  }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,22 @@
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-import org.junit.Test;
+import org.junit.*;
 
 public class HelloWorldTest {
+  private HelloWorld hw;
+
+  @Before
+  public void before() {
+    hw = new HelloWorld();
+  }
+
   @Test
   public void testHello() {
-    HelloWorld hw = new HelloWorld();
     assertThat("HelloWorld.hello test", "Hello, World!", is(hw.hello()));
+  }
+
+  @Test
+  public void testStringEqualsAbc() {
+    assertThat("HelloWorld.stringEqualsAbc test", true, is(hw.stringEqualsAbc("abc")));
   }
 }
